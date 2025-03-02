b58eccf611b07cc685c8d9dd4f6c2c633fcb7ef0
Gypra <gytisnaujasis@icloud.com>
2025-03-02 11:43:00 +0200

    Add error checking for not found brands

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,13 +6,20 @@ exports.getProductsByBrand = async (req, res) => {
     let brandAggList = await getBrandAgg();
     let products = await getProducts();
 
-    products = getBrandsProducts(products, brandAggList);
+    const brandsObj = getBrandsProducts(products, brandAggList);
+    
+    if (!Object.keys(brandsObj).length){
+        res.status(404).json({
+            status: "failed",
+            message: "No brands and products were found"
+        });
+        return;
+    }
 
     res.status(200).json({
         status: "success",
         data: {
-            result_count: products.length,
-            result: products,
+            result: brandsObj,
         }
-    })
+    });
 }
