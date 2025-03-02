e182d1d9165d302ea74204154a0fdd500b15a369
Gypra <gytisnaujasis@icloud.com>
2025-03-02 18:47:22 +0200

    Update insert

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,3 +1,14 @@
+CREATE DATABASE "products-brand"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en-US'
+    LC_CTYPE = 'en-US'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
 DROP TABLE products;
 CREATE TABLE IF NOT EXISTS "products"(
 	id SERIAL PRIMARY KEY,
